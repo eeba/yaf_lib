371214391274a78a31b5bc3da4fc310df80546e0
eeba <wenlongde@gmail.com>
2017-09-23 23:57:49 +0800

    去掉验证

--- Common/AdminAbstract.php
+++ Common/AdminAbstract.php
@@ -4,12 +4,4 @@ namespace Common;
 abstract class AdminAbstract extends Controller {
     protected $route = 'simple';
 
-    public function init() {
-        parent::init();
-        $app_info = $_SESSION['admin_info'];
-        if (!$app_info) {
-            $this->redirect('/admin/login/index');
-        }
-    }
-
 }
