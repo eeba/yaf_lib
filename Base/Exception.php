354fda749b4bf1c7923f05681fc653a4820d1600
eeba <wenlongde@gmail.com>
2017-10-26 11:18:19 +0800

    错误信息一律读msg, 不再分user_msg和msg

--- Base/Exception.php
+++ Base/Exception.php
@@ -5,7 +5,7 @@ class Exception extends \Exception {
 
     public function __construct($message = "", $code = 0, \Exception $previous = null) {
         $msg = Config::get($message);
-        $message = isset($msg['user_msg']) ? $msg['user_msg'] : $message;
+        $message = isset($msg['msg']) ? $msg['msg'] : $message;
         $code = isset($msg['code']) ? $msg['code'] : $code;
         parent::__construct($message, $code, $previous);
     }
