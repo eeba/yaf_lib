f2b3f95e2fb216abab07ffcc21054245730782cd
eeba <wenlongde@gamil.com>
2017-11-28 15:48:06 +0800

    url验证

--- Validate/Type/Url.php
+++ Validate/Type/Url.php
@@ -7,15 +7,9 @@ use Base\Exception;
  * 验证url
  */
 class Url extends \Validate\Abstraction {
-    protected $_default_settings = array(
-        'filter_xss' => true,
-        'is_trusted' => false,   // 是否为可信的回跳地址
-        'trusted_domains' => array(), // 自定义的一些可信域
-    );
-
     public function action($param) {
-        $result = filter_var($param['value'], FILTER_VALIDATE_URL);
-        if (!$result) {
+        $parse = parse_url($param['value']);
+        if (!$parse || !isset($parse['host']) || !in_array(strtolower($parse['scheme']), array('ftp','http', 'https'))) {
             if ($param['msg']) {
                 throw new Exception($param['msg']);
             } else {
@@ -24,11 +18,5 @@ class Url extends \Validate\Abstraction {
         }
 
         return $param['value'];
-    }
-
-    public function filter_xss($url) {
-        $illegal_chars = array('\'', '"', ';', '<', '>', '(', ')', '{', '}', '[', ']');
-        $encoded_chars = array('%27', '%22', '%3B', '%3C', '%3E', '%28', '%29', '%7B', '%7D', '%5B', '%5D');
-        return str_replace($illegal_chars, $encoded_chars, $url);
     }
 }
