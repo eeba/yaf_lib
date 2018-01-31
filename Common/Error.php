261e68bbb3175d173788f70be1d3e51faa7f3d07
eeba <wenlongde@gamil.com>
2018-01-31 13:19:00 +0800

    添加图片处理库

--- Common/Error.php
+++ Common/Error.php
@@ -29,6 +29,7 @@ class Error extends \Common\Controller {
      * @param $exception
      */
     public function errorAction($exception) {
+        Logger::getInstance('request')->debug([$exception]);
         $is_not_found = $this->isNotFound($exception->getCode());
         //        print_r($this->getRequest());
         if ($is_not_found) {
