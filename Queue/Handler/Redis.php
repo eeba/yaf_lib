599e01e32917ec37dc7bd05a5da5d2857faea3c8
eeba <wenlongde@gamil.com>
2018-02-28 18:40:00 +0800

    加redis队列，优化cache

--- Queue/Handler/Redis.php
+++ Queue/Handler/Redis.php
@@ -14,17 +14,21 @@ class Redis extends Abstraction {
     }
 
     public function push($queue_name, $message, $option = array()) {
-        $ret = $this->redis->rPush($queue_name, $message);
+        $ret = $this->redis->rPush($this->getKey($queue_name), $message);
         return $ret;
     }
 
     public function pop($queue_name, $option = array()) {
-        $ret = $this->redis->lPop($queue_name);
+        $ret = $this->redis->lPop($this->getKey($queue_name));
         return $ret;
     }
 
     public function len($queue_name) {
-        $ret = $this->redis->lLen($queue_name);
+        $ret = $this->redis->lLen($this->getKey($queue_name));
         return $ret;
+    }
+
+    public function getKey($key){
+        return 'QUEUE_' . strtoupper($key);
     }
 }
