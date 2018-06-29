91ccbe8559282a15f30b728448049f5551063585
eeba <wenlongde@gmail.com>
2018-06-29 18:14:14 +0800

    常量名修改，目录调整

--- Thread/Config.php
+++ Thread/Config.php
@@ -138,7 +138,6 @@ class Config {
      * @return bool
      */
     private function _setConfigByFile() {
-        var_dump(json_encode($this->_config));
         if (md5(json_encode($this->_config)) !== md5_file(self::$_thread_config_file)) {
             file_put_contents(self::$_thread_config_file, json_encode($this->_config));
         }
