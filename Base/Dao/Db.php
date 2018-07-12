8406d81f288a2e0f93e2d06931866883153c5699
eeba <wenlongde@gmail.com>
2018-07-12 11:35:26 +0800

    添加注释

--- Base/Dao/Db.php
+++ Base/Dao/Db.php
@@ -41,14 +41,23 @@ class Db {
         return $data;
     }
 
+    /**
+     * 事务开始
+     */
     public function begin() {
         $this->db()->begin();
     }
 
+    /**
+     * 提交事务
+     */
     public function commit() {
         $this->db()->commit();
     }
 
+    /**
+     * 回滚
+     */
     public function rollback() {
         $this->db()->rollback();
     }
@@ -63,31 +72,86 @@ class Db {
         return $this->db()->insert($this->table, $data);
     }
 
+    /**
+     * 更新数据
+     *
+     * @param array $data
+     * @param array $where
+     * @param array $order
+     * @param int   $limit
+     * @return bool|int|null
+     */
     public function update(array $data = array(), array $where = array(), array $order = array(), $limit = 0) {
         return $this->db()->update($this->table, $data, $where, $order, $limit);
     }
 
+    /**
+     * 根据主键id，查一条数据
+     *
+     * @param        $id
+     * @param string $cols
+     * @param array  $order
+     * @return array
+     */
     public function findById($id, $cols = '*', array $order = array()) {
         return $this->db()->find($this->table, ['id' => $id], $cols, $order);
     }
 
+    /**
+     * 查一条数据
+     *
+     * @param array  $where
+     * @param string $cols
+     * @param array  $order
+     * @return array
+     */
     public function find($where = [], $cols = '*', array $order = array()) {
         return $this->db()->find($this->table, $where, $cols, $order);
     }
 
+    /**
+     * 查多条数据
+     *
+     * @param array  $where
+     * @param string $cols
+     * @param array  $order
+     * @param int    $limit
+     * @return bool|int|null
+     */
     public function getList(array $where = array(), $cols = '*', array $order = array(), $limit = 0) {
         return $this->db()->select($this->table, array_filter($where), $cols, $order, $limit);
     }
 
+    /**
+     * 直接执行sql
+     *
+     * @param       $sql
+     * @param array $params
+     * @return bool|int|null
+     */
     public function query($sql, $params = []) {
         return $this->db()->query($sql, $params);
     }
 
+    /**
+     * 统计数量
+     *
+     * @param array $where
+     * @return mixed
+     */
     public function count($where = []) {
         $ret = $this->find($where, 'count(1) num');
         return $ret['num'];
     }
 
+    /**
+     * 和JQuery DataTables插件配合使用
+     *
+     * @param array  $where
+     * @param string $cols
+     * @param array  $order
+     * @return mixed
+     */
     public function dataTable(array $where = [], $cols = '*', array $order = []) {
         $sql = "select {$cols} from {$this->table} where ";
         $count_sql = "select count(1) num from {$this->table} where ";
