33ff2d1da5e1a389484472810075de54ecd8f99c
eeba <wenlongde@gmail.com>
2018-07-20 10:23:09 +0800

    修改模板中的统计插件， 添加频率限制插件

--- Build/template/application/plugins/Statistic.php
+++ Build/template/application/plugins/Statistic.php
@@ -4,9 +4,9 @@ class Plugin_Statistic extends \Yaf\Plugin_Abstract{
 
     //访问量统计
     public function dispatchLoopStartup(\Yaf\Request_Abstract $request, \Yaf\Response_Abstract $response) {
-        $url = $request->getRequestUri();
+        $uri = $request->getModuleName() . '/' .$request->getControllerName() . '/' .$request->getActionName();
         $redis = new \Db\Redis();
-        $redis->hIncrBy('STATISITC'.date('Ymd'), $url, 1);
+        $redis->hIncrBy('STATISITC'.date('Ymd'), $uri, 1);
     }
 
 }
