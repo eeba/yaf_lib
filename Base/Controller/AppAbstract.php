d00f0af3cafe848aed58d74efcb9257681ce9448
eeba <wenlongde@gmail.com>
2018-07-25 17:49:56 +0800

    一个不是必要的抽象方法

--- Base/Controller/AppAbstract.php
+++ Base/Controller/AppAbstract.php
@@ -4,6 +4,11 @@ namespace Base\Controller;
 use Http\Response;
 
 abstract class AppAbstract extends PlainAbstract  {
+
+    public function params() {
+        return [];
+    }
+
     public function init() {
         parent::init();
         Response::setFormatter(Response::FORMAT_HTML);
