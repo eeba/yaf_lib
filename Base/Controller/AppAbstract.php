3afe18299927a88aa0460df4d994b0454a7cdcca
eeba <wenlongde@gmail.com>
2018-08-08 16:56:30 +0800

    优化基础类

--- Base/Controller/AppAbstract.php
+++ Base/Controller/AppAbstract.php
@@ -1,22 +1,15 @@
 <?php
 namespace Base\Controller;
 
-use Http\Response;
-
 abstract class AppAbstract extends Controller  {
     protected $route = 'static';
 
     protected $params;
 
     public function getParam($key, $default=''){
-        return isset($this->params[$key]) ? $this->params[$key] : $default;
-    }
-
-    public function init() {
-        parent::init();
-        Response::setFormatter(Response::FORMAT_HTML);
-
         //PATH_INFO中的参数
-        $this->params = $this->getRequest()->getParams();
+        $path_info_params = $this->getRequest()->getParams();
+        $params = array_merge($path_info_params, $_REQUEST);
+        return isset($params[$key]) ? $params[$key] : $default;
     }
 }
