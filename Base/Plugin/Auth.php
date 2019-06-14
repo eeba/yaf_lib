791d4b461fa631c9eae6619f04d65cae2c18611c
eeba <wenlongde@gmail.com>
2019-06-14 18:52:40 +0800

    添加Admin模块

--- Base/Plugin/Auth.php
+++ Base/Plugin/Auth.php
@@ -9,6 +9,7 @@ class Auth extends \Yaf\Plugin_Abstract{
         '/admin/login/index',
         '/admin/login/dologin',
         '/admin/login/captcha',
+        '/admin/index/index',
     ];
     /**
      * Admin模块访问权限
@@ -22,8 +23,14 @@ class Auth extends \Yaf\Plugin_Abstract{
     public function dispatchLoopStartup(\Yaf\Request_Abstract $request, \Yaf\Response_Abstract $response) {
         $uri = strtolower($request->getRequestUri());
         $admin_acl = isset($_SESSION['admin_acl']) ? $_SESSION['admin_acl'] : [];
-        if(!in_array($uri, $admin_acl) && !in_array($uri, $this->default_acl)){
-            throw new Exception("您没有<code>{$uri}</code>访问权限");
+        if(!in_array($uri, $this->default_acl)){
+            if (!isset($_SESSION['admin_info'])) {
+                throw new Exception("您已退出，请先<a onclick='parent.window.location.reload();' style='cursor: pointer;'>登录</a>");
+            }
+
+            if (!in_array($uri, $admin_acl)) {
+                throw new Exception("您没有<code>{$uri}</code>访问权限");
+            }
         }
     }
 
