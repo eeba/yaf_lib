4325ae3e0c3098fea01a52ce62644799faf7c716
eeba <wenlongde@gmail.com>
2019-06-24 18:42:35 +0800

    添加Admin模块

--- S/Cache/Handler/Redis.php
+++ S/Cache/Handler/Redis.php
@@ -1,17 +1,13 @@
 <?php
 namespace S\Cache\Handler;
 
-use S\Db\Redis as DbRedis;
+use Base\Exception;
 
 /**
  * Class \Cache\Redis
  */
 class Redis extends Abstraction {
     protected $redis = null;
-
-    protected function init() {
-        $this->redis = new DbRedis();
-    }
 
     /**
      * get
@@ -78,5 +74,60 @@ class Redis extends Abstraction {
         return true;
     }
 
-    public function getInstance() {}
+    /**
+     * @return \Redis|null
+     * @throws Exception
+     */
+    public function getInstance() {
+        if (!$this->redis) {
+            $this->redis = new \Redis();
+            $this->connect();
+            $this->setOptions();
+        }
+
+        return $this->redis;
+    }
+
+    /**
+     * 初始化配置信息，以addServers方式使用
+     *
+     * @return bool
+     * @throws Exception
+     */
+    protected function connect() {
+        if (isset($this->config['persistent']) && $this->config['persistent']) {
+            $this->_persistent = true;
+            $conn = $this->redis->pconnect($this->config['host'], $this->config['port'], $this->config['timeout'] ?: self::DEFAULT_CONNECT_TIMEOUT);
+            //重连一次
+            if ($conn === false) {
+                $conn = $this->redis->pconnect($this->config['host'], $this->config['port'], $this->config['timeout'] ?: self::DEFAULT_CONNECT_TIMEOUT);
+            }
+        } else {
+            $conn = $this->redis->connect($this->config['host'], $this->config['port'], $this->config['timeout'] ?: self::DEFAULT_CONNECT_TIMEOUT);
+            //重连一次
+            if ($conn === false) {
+                $conn = $this->redis->connect($this->config['host'], $this->config['port'], $this->config['timeout'] ?: self::DEFAULT_CONNECT_TIMEOUT);
+            }
+        }
+
+        if ($conn === false) {
+            throw new Exception("redis connect " . $this->config['host'] . " fail");
+        }
+
+        return true;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function setOptions() {
+        if (isset($this->config['user']) && $this->config['user'] && $this->config['auth']) {
+            if ($this->redis->auth($this->config['user'] . ":" . $this->config['auth']) == false) {
+                throw new Exception("redis auth " . $this->config['host'] . " fail");
+            }
+        }
+        if (isset($this->config['db']) && $this->config['db']) {
+            $this->redis->select($this->config['db']);
+        }
+    }
 }
