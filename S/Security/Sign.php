5a60b34e5e0d2b6abb80c3460eca2429899444ab
eeba <wenlongde@gmail.com>
2019-08-04 14:21:32 +0800

    优化

--- S/Security/Sign.php
+++ S/Security/Sign.php
@@ -1,6 +1,8 @@
 <?php
 namespace S\Security;
 
+use Base\Logger;
+
 class Sign {
     /**
      * 生成请求时所需的签名串
@@ -15,6 +17,7 @@ class Sign {
     public static function getSign($app_key, $app_secret, $time, array $params = array()){
         unset($params['app_key'], $params['app_secret'], $params['t'], $params['m']);
         ksort($params, SORT_STRING);
+        Logger::getInstance()->debug(['sign_data' => $params]);
         $sign = $app_key.$app_secret.$time.implode('', $params);
         return md5($sign);
     }
