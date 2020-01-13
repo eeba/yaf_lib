58c15024933470483ce080d736745ebec1b80c5b
eeba <wenlongde@gmail.com>
2020-01-13 17:15:15 +0800

    验证

--- S/Validate/Type/Strzh.php
+++ S/Validate/Type/Strzh.php
@@ -7,7 +7,7 @@ use S\Validate\Abstraction;
 class Strzh extends Abstraction {
 
     public function action($param) {
-        if (preg_match('/^(?:[\u4e00-\u9fa5·]{2,16})$/', $param['value'])) {
+        if (preg_match('/^([\u4e00-\u9fa5·]{2,16})$/', $param['value'])) {
             return $param['value'];
         }
 
